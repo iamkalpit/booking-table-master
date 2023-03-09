01a51372f9e33c31d1009a03fdd6d832a704f6d3
Kalpit Rastogi <me@iamkalpit.com>
2023-03-10 02:12:32 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Nav from "./components/Nav";
+import Header from "./components/Header";
+import BookingForm from "./components/BookingForm";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <main>
+      <Nav />
+      <Header />
+      <BookingForm />
+      <Footer />
+    </main>
+  );
+}
+
+export default App;
